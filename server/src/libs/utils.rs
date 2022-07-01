4963af3ec9fb12ae2f20204104ffffbea05ea924
Coombszy <LCCoombs@hotmail.co.uk>
2022-07-01 17:56:24 +0100

    Add utils and move configs

---
+++ server/src/libs/utils.rs
@@ -0,0 +1,36 @@
+use std::{fs, process::exit};
+
+use log::error;
+
+use crate::libs::structs::TOMLData;
+
+// Loads TOMLData struct from filename
+pub fn load_config_toml(filename: String) -> TOMLData {
+    // Load in raw string from config toml
+    let toml_raw = match fs::read_to_string(&filename) {
+        Ok(c) => c,
+        // Failed to read file
+        Err(e) => {
+            error!("Could not read TOML file '{}'", &filename);
+            error!("Error: {}", e);
+            exit(1);
+        }
+    };
+    // Convert to TOML struct
+    let config_data: TOMLData = match toml::from_str(&toml_raw) {
+        Ok(d) => d,
+        // Failed to parse from String to TOMLData Struct
+        Err(e) => {
+            error!("Unable to load data from {}", &filename);
+            error!("Error: {}", e);
+            exit(1);
+        }
+    };
+    config_data
+}
+
+pub fn send_magic_packet(mac_address: &[u8; 6]) -> Result<(), std::io::Error> {
+    let magic_packet = wake_on_lan::MagicPacket::new(mac_address);
+
+    magic_packet.send()
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -9,42 +9,53 @@ use dotenv::dotenv;
 use log::{debug, error, info, LevelFilter};
 use simplelog::*;
 
-use std::fs::{self, File};
+use std::fs::File;
 use std::process::exit;
 use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 use std::{env, str::FromStr};
+
+use crate::libs::utils::load_config_toml;
 
 const DATA_FOLDER: &str = "config/";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    startup();
+    let toml_data: TOMLData = startup();
+
+    // Load Notifications
+    // if toml_data.config.schedule_enabled {
+    //     let notifications: Vec<Notification> =
+    //         load_notifications(&toml_data.config.schedule_source);
+    //     info!("Notifications loaded: {}", notifications.len());
+
+    //     // Create scheduled notifications
+    //     notification_scheduler(&notifications, toml_data.config.clone());
+    // }
 
     // Start Web
-    let host: String = "0.0.0.0".to_string();
-    let port: u16 = 4050;
-    info!("Starting web server, listening on {host}:{port}");
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(State {
-                start_time: Utc::now(),
-            }))
-            .service(libs::routes::health)
-    })
-    .bind((host, port))?
-    .run()
-    .await
-
-    // Testing
-    /*
-    let mac_address: [u8; 6] = [0x00, 0x23, 0x24, 0x82, 0x10, 0x46];
-
-    let magic_packet = wake_on_lan::MagicPacket::new(&mac_address);
-
-    magic_packet.send();
-
-    println!("Sent!");
-    */
+    if toml_data.config.web_enabled {
+        let host: String = toml_data.clone().config.web_host;
+        let port: u16 = toml_data.clone().config.web_port;
+        info!("Starting web server, listening on {host}:{port}");
+        HttpServer::new(move || {
+            App::new()
+                .app_data(web::Data::new(State {
+                    start_time: Utc::now(),
+                }))
+                .service(libs::routes::health)
+        })
+        .bind((host, port))?
+        .run()
+        .await
+    }
+    // Keep app alive
+    else {
+        loop {
+            thread::sleep(Duration::from_secs(10));
+        }
+    }
 }
 
 fn startup() -> TOMLData {
@@ -102,29 +113,4 @@ fn startup() -> TOMLData {
     }
 
     toml_data
-}
-
-// Loads TOMLData struct from filename
-pub fn load_config_toml(filename: String) -> TOMLData {
-    // Load in raw string from config toml
-    let toml_raw = match fs::read_to_string(&filename) {
-        Ok(c) => c,
-        // Failed to read file
-        Err(e) => {
-            error!("Could not read TOML file '{}'", &filename);
-            error!("Error: {}", e);
-            exit(1);
-        }
-    };
-    // Convert to TOML struct
-    let config_data: TOMLData = match toml::from_str(&toml_raw) {
-        Ok(d) => d,
-        // Failed to parse from String to TOMLData Struct
-        Err(e) => {
-            error!("Unable to load data from {}", &filename);
-            error!("Error: {}", e);
-            exit(1);
-        }
-    };
-    config_data
 }
